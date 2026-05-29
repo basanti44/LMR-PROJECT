f8dcc768513a6cdf5fe16b4f134928132f03920b
Basanti <basantikumari8800@gamil.com>
2026-05-29 14:49:24 +0530

    fixed course details api

--- client/src/pages/CourseDetails.jsx
+++ client/src/pages/CourseDetails.jsx
@@ -22,8 +22,8 @@ function CourseDetails() {
       return;
     }
 
-    axios
-      .get(`http://localhost:5000/api/courses/${id}`)
+    
+    axios.get(`https://lmr-backend-aeja.onrender.com/api/courses/${id}`)
       .then((res) => {
 
         if (res.data) {
